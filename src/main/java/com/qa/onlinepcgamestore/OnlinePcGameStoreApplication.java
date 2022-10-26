7ee9b42e1dba8bf33abca58e2dbdf29d275aecca
AlexTJ <alextj160398@gmail.com>
2022-10-26 10:34:00 +0100

    initial commit

---
+++ src/main/java/com/qa/onlinepcgamestore/OnlinePcGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.qa.onlinepcgamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePcGameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePcGameStoreApplication.class, args);
+	}
+
+}
